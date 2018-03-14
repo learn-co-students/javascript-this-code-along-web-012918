4e87fe3baa173780dbc0b5f0608a86a554727dff
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-14 13:07:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  this.serve = function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
+blt.name = "BLT";
+blt.serve();
