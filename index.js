24bd340f42aadd23c49dc3a25d3c48db45ad37ab
izzee <ichabon@me.com>
2018-03-19 16:18:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much."
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+}
+
+function serve() {
+    if(arguments.length > 0) {
+        var customers = Array.prototype.slice.call(arguments);
+        last = customers.pop();
+        console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
+    }else {
+        console.log(this.name + ". Order up!");
+    }
+}
+
+function deliverFood(customer, table) {
+  console.log("Delivering " + this.name + " to " + customer + " at table " + table);
+}
+
+
+
+const gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+const pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
